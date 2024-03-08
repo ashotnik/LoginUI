869a5d054f5f2cb1e8b03ca9fbc8d93efd8c7879
AshotNik <ashot.nik777@gmail.com>
2024-03-08 15:17:04 +0400

    Shared Prefernces

--- lib/db/preferences_service.dart
+++ lib/db/preferences_service.dart
@@ -1,21 +1,11 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-// SOLID
-// KISS
-// YAGNI
-// Singleton
-// Service Locator vs Dependency Injection
 
 class PreferencesService {
   final SharedPreferences preferences;
 
   const PreferencesService(this.preferences);
 
-  // Second Variant to get instance
-  // Future<SharedPreferences> initPreferences() async {
-  //   final SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   return prefs;
-  // }
 
   Future<void> setphoneNumber(String phoneNumber) async {
     await preferences.setString('phoneNumber', phoneNumber);
